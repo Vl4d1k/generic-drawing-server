5d1b2e2de56efb182444b52ef6b1ba7212801d95
Vlad <vlad1k9369@gmail.com>
2022-04-02 18:41:42 +0300

    create upload image route

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
     print(request.files)
@@ -18,7 +16,10 @@ def upload_image():
         return 'there is no image in form!'
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.mkdir(app.config['UPLOAD_FOLDER'])
+    
+    pid = uuid.uuid4()
+    uploadedImage.filename = pid
     path = os.path.join(app.config['UPLOAD_FOLDER'], uploadedImage.filename)
     uploadedImage.save(path)
-    pid = uuid.uuid4()
+    
     return jsonify(pid=pid)
