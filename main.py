c12b6ab458087b173531a27d1ac14f5f1a0c6c1a
a.stefanov <a.stefanov@crpo.su>
2022-04-02 22:38:27 +0300

    добавил отдачу изображений

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os.path
 import uuid
 from flask import Flask, request, jsonify
 
 from GeneratorThread import *
 
 UPLOAD_FOLDER = 'storage/uploads'
+GENERATE_FOLDER = 'storage/generated'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 threadsPool = []
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['GENERATE_FOLDER'] = GENERATE_FOLDER
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
@@ -28,3 +31,34 @@ def upload_image():
     th.start()
     
     return jsonify(pid=pid)
+
+@app.route('/images/preview', methods=['GET'])
+def images_preview():
+    dir = os.scandir(app.config['UPLOAD_FOLDER'])
+    result = []
+
+    for file in dir:
+        if not file.is_file() or file.name == '.gitkeep':
+            continue
+        uid = os.path.splitext(file.name)[0]
+        result.append({
+            "image": f'img/uploads/{uid}',
+            "id": uid
+        })
+
+    return jsonify(result)
+
+@app.route('/images/<string:uuid>', methods=['GET'])
+def images_get(uuid):
+    dir = os.scandir(os.path.join(app.config['GENERATE_FOLDER'], uuid))
+    result = []
+
+    for file in dir:
+        if not file.is_file():
+            continue
+        fileName = os.path.splitext(file.name)[0]
+        result.append(int(fileName))
+
+    result = sorted(result)
+
+    return jsonify(list(map(lambda name: f'img/generated/{uuid}/{name}', result)))
